4826c2ad693bf56bb4f48b2510a8ee8d93b18bc7
Avis <aapvdev@gmail.com>
2026-06-23 23:21:34 +0800

    Fix property signal retrieval in fireSignalsForProp

--- LimbExtender.lua
+++ LimbExtender.lua
@@ -114,7 +114,7 @@ end
 
 local function fireSignalsForProp(limb, prop)
 	firesignal(limb.Changed, prop)
-	local sig = limb:GetPropertyChangedSignal(prop)
+	local sig = limb:(prop)
 	firesignal(sig)
 end
 
